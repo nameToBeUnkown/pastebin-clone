d17532b62fe10c71322f800227bfc79dcd1a9c8d
DinGis9 <oleg777gorbachov@gmail.com>
2026-05-16 12:23:21 +0300

    fix: image for github profile

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */ 
   serverExternalPackages: ["better-sqlite3"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
